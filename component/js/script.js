b4e615c8d1dc956368f8e613679dcd5b61e997d9
dasrajanraj <42641430+dasrajanraj@users.noreply.github.com>
2019-11-08 21:50:32 +0530

    main page fixing

---
+++ component/js/script.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+  document.getElementById("mySidenav").style.zIndex="1000"
+  document.getElementById("main").style.marginRight = "350px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+  document.body.style.backgroundColor = "white";
+}
+
+function popScr(){
+  console.log("dev is clicked");
+}
+
---
+++ component/pages/timeline/js/data.js
@@ -0,0 +1,124 @@
+var codropsEvents = {
+	/*'11-21-2019' : '<a href="http://www.wincalendar.com/Great-American-Smokeout">Great American Smokeout</a>',
+	'11-13-2013' : '<span>Ashura [An example of an complete date event (11-13-2013)]</span>',
+	'11-11-2013' : '<a href="http://www.wincalendar.com/Remembrance-Day">Remembrance Day (Canada)</a>',
+	'11-04-2013' : '<span>Islamic New Year</span>',
+	'11-03-2013' : '<a href="http://www.wincalendar.com/Daylight-Saving-Time-Ends">Daylight Saving Time Ends</a>',
+	'11-01-2013' : '<span>All Saints Day</span>',
+	'12-25-2019' : '<span>Christmas Day [Date : 12-25-2019]</span>',
+	'01-01-YYYY' : '<span>New Year\'s Eve (Event repeats every YEAR)</span>',
+	'MM-02-2013' : '<span>Yeah, Monthly [MM-02-2013]</span>',
+	'MM-07-YYYY' : '<span>[MM-07-YYYY], Monthly and Yearly</span>',
+	'11-DD-2014' : {content : '<span> {\'11-DD-2014\' : {content : \'Something\', endDate : 20}}</span>', endDate : 20},
+	'02-DD-2014' : {content : '<span>Ex: {\'02-DD-2014\' : {content : \'Something\', startDate : 10, endDate : 20}}</span>', startDate : 10, endDate : 20},
+	'12-DD-YYYY' : '<span>[12-DD-YYYY] Whole month and Year</span>',
+	'TODAY' : '<span>Today, [Date : \'TODAY\']</span>',
+	'10-16-2014': ['<a href="">event one</a>', '<span>event two</span>'],
+	'10-DD-YYYY' : [
+		{content : '<span> {\'10-DD-2014\' : {content : \'Something\', startDate : 10, endDate : 20}}</span>', startDate : 10, endDate : 20},
+		{content : '<span>Ex: {\'10-DD-2014\' : {content : \'Something\', endDate : 20}}</span>', endDate: 20}],
+*/
+'03-07-2019':[
+ {content : '<span><p>OLYMPIC TRY OUTS&nbsp&nbsp&nbsp&nbspBASKETBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM,INDOOR<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2pm TO 7pm</p></span>'}
+ 
+ ],
+
+'03-08-2019':[
+{content:'<span><p>FLASH MOB/THEME DAY 1&nbsp&nbsp&nbsp&nbsp&nbsp&nbspALL THE BLOCKS<br>TIME: 10am TO 12</p></span>'},
+{content : '<span><p>OLYMPIC TRY OUTS&nbsp&nbsp&nbsp&nbspBASKETBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM,INDOOR<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2pm TO 7pm</p></span>'}
+ ],
+
+'03-09-2019':[
+ {content:'<span><p>AFL JERSEY LAUNCH&nbsp&nbsp&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM<br>TIME: 10am TO 12</p></span>'},
+ {content : '<span><p>OLYMPIC TRY OUTS&nbsp&nbsp&nbsp&nbspBASKETBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM,INDOOR<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2pm TO 7pm</p></span>'}
+ 
+ ],
+'03-10-2019':[
+ {content : '<span><p>OLYMPIC TRY OUTS&nbsp&nbsp&nbsp&nbspBASKETBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM,INDOOR<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 9am TO 7pm</p></span>'}
+ 
+ ],
+ '03-11-2019':[
+{content:'<span><p>AFL(Till 16th March)&nbsp&nbsp&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT<br>TIME: 4 TO 9</p></span>'},
+{content:'<span><p>OPEN AIR &nbsp&nbsp&nbsp&nbsp&nbsp&nbspMBA OPEN ARENA<br>TIME: 2pm onwards</p></span>'}
+],
+ '03-12-2019':[
+{content : '<span><p>OLYMPICS DAY 1&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2pm TO 7pm,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2pm to 4pm</p></span>'}
+],
+ '03-13-2019':[
+{content : '<span><p>OLYMPICS DAY 2&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2pm TO 7pm,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2pm to 4pm</p></span>'}
+],
+ '03-14-2019':[
+ {content : '<span><p>OLYMPICS DAY 3&nbsp&nbsp&nbsp&nbspBASKETBALL COURT,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM,INDOOR<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: 2PM TO 7pm</p></span>'}
+],
+'03-15-2019':[
+ {content : '<span><p>FGT&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMAIN AUDITORIUM/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTAGE NEAR CLOCK TOWER</p><p><br>TIME: 11am TO 7pm</p></span>'},
+ {content:'<span><p>APL JERSEY LAUNCH&nbsp&nbsp&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM<br>TIME: 10am TO 12</p></span>'}
+],
+
+'03-18-2019':[
+ {content : '<span><p>HABBATHON</p><p><br>TIME: 8am TO 11am</p></span>'},
+ {content : '<span><p>APL START(Till 23rd March)&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p><br>TIME: WHOLE DAY</p></span>'}
+
+ ],
+
+'03-19-2019':[
+ {content : '<span><p>AGT&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMAIN AUDITORIUM/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTAGE NEAR CLOCK TOWER</p><p>TIME: 2pm onwards</p></span>'},
+ {content:'<span><p>OPEN AIR &nbsp&nbsp&nbsp&nbsp&nbsp&nbspMBA OPEN ARENA<br><br>TIME: 2pm to 5pms</p></span>'}
+],
+
+
+'03-20-2019':[
+ {content : '<span><p>COMIC CON-THEME DAY</p><p>TIME: 2 TO 8</p></span>'},
+ {content : '<span><p>OMG</p><p>TIME: 2 TO 4</p></span>'},
+ {content : '<span><p>COSPLAY,MOVIE&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMBA OPEN ARENA,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspBASKETBALL COURT</p><p>TIME:3pm TO 5pm,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp5pm to 8pm</p></span>'}
+
+ ],
+
+'03-22-2019':[
+ {content : '<span><p>HOGGATHON&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspCANTEEN,<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPATHWAY</p><p>TIME: 1pm to 3pm</p></span>'}
+ 
+],
+
+'03-23-2019':[
+ {content : '<span><p>SILENT DISCO&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspINDOOR STADIUM</p><p>TIME: 2pm to 8pm</p></span>'},
+ {content : '<span><p>Mr AND Ms HABBA&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMAIN AUDITORIUM</p><p>TIME:11am to 3pm</p></span>'}
+],
+
+'03-25-2019':[
+ {content : '<span><p>ETHNIC DAY-THEME DAY 3</p><p>TIME:FULL DAY</p></span>'},
+ {content : '<span><p>CARNIVAL&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSTADIUM</p><p>FULL DAY</p></span>'},
+ {content : '<span><p>Mr AND Ms ETHNIC&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp STADIUM/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFRONT OF MBA</p><p>TIME:2PM to 5pm</p></span>'},
+ {content : '<span><p>FACULTY FASHION SHOW&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp STADIUM/<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFRONT OF MBA</p><p>TIME:2PM to 5pm</p></span>'}
+
+],
+'03-26-2019':[
+ {content : '<span><p>MeSTORI&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMAIN AUDITORIUM</p><p>TIME:FULL DAY</p></span>'},
+ {content : '<span><p>LAGORI&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspVOLLEYBALL COURT</p><p>TIME:1PM TO 3PM</p></span>'},
+ {content : '<span><p>SLOW BIKE RACE&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAIGS-POLYTECHNIC</p><p>TIME:3pm to 5pm</p></span>'},
+ {content : '<span><p>BEG BORROW STEAL&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp CS BLOCK</p><p>TIME:9am to 3pm</p></span>'},
+ {content : '<span><p>DESIGNERS CALL&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp MBA AUDITORIUM</p><p>TIME:11am to 3pm</p></span>'}
+
+],
+'03-27-2019':[
+ {content : '<span><p>BATTLE OF BANDS&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspBASKETBALL COURT</p><p>TIME:4pm TO 8pm</p></span>'},
+ {content : '<span><p>TRESURE HUNT&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspCS BLOCK</p><p>TIME:9am onwards</p></span>'},
+ {content : '<span><p>SOLO SINGING&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMBA ARENA</p><p>TIME:9:30am to 12:30pm</p></span>'},
+ {content : '<span><p>LA SENORITA EL SENOR&nbsp&nbsp&nbspMAIN AUDITORIUM</p><p>TIME:9am to 5pm</p></span>'},
+ {content : '<span><p>DUET SINGING&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp MBA ARENA</p><p>TIME:12:30pm to 4pm</p></span>'}
+
+],
+
+'03-02-2019':[
+ {content : '<span><p>APL AUCTION&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMAIN AUDITORIUM</p><p>TIME: 9am onwards</p></span>'}
+ 
+],
+'03-05-2019':[
+ {content : '<span><p>ACHARYA PRO KABBADI LEAGUE&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<br>VOLLEYBALL COURT</p><p>TIME:WHOLE DAY</p></span>'}
+ 
+],
+'03-06-2019':[
+ {content : '<span><p>ACHARYA PRO KABBADI LEAGUE&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<br>VOLLEYBALL COURT</p><p>TIME:WHOLE DAY</p></span>'}
+ 
+],
+
+};
